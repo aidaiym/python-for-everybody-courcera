c3a141e35f7e145b45e05dc3a56f68aa4d3e63a5
Aidaiym <omurbekovnaaidaiym@gmail.com>
2021-11-23 11:22:09 +0600

    first commit

---
+++ second_lesson.py
@@ -0,0 +1,22 @@
+
+#  Python
+#  Reserved words
+
+#  False	def	if	raise
+#  None	del	import	return
+#  True	elif	in	try
+#  and	else	is	while
+#  as	except	lambda	with
+#  assert	finally	nonlocal	yield
+#  break	for	not	
+#  class	from	or	
+#  continue	global	pass
+
+# Variables rules
+
+# A variable name must start with a letter or the underscore character.
+# A variable name cannot start with a number.
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+# Variable names are case-sensitive (name, Name and NAME are three different variables).
+# The reserved words(keywords) cannot be used naming the variable.
+
